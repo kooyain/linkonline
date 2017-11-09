e44a21c05457ebdbfd62fad16dc99679832618f7
lihz <lihongzhen@icexpert.biz>
2017-11-09 18:28:12 +0800

    no build

--- src/main.go
+++ src/main.go
@@ -1,32 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
-	router.HandleFunc("/todos", TodoIndex)
-	router.HandleFunc("/todos/{todoid}", TodoShow)
+	router := NewRouter()
 
 	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello")
-}
-
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "TodoIndex")
-}
-
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	todoId := vars["todoid"]
-	fmt.Fprintf(w, "TodoShow: ", todoId)
 }
